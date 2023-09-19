4cf2c8f0d6221bc500b882e11d68cdf1d2c9f649
jlw4958 <jlw4958@g.rit.edu>
2023-09-19 18:14:49 -0400

    problems in Game.cpp
    VertexShaderExternalData no longer has offset value, shapes aren't drawing on the screen

--- GameEntity.cpp
+++ GameEntity.cpp
@@ -1,1 +1,21 @@
 #include "GameEntity.h"
+#include "Mesh.h"
+#include <iostream>
+
+GameEntity::GameEntity(std::shared_ptr<Mesh> mesh_ptr)
+{
+	myMesh = mesh_ptr;
+}
+
+std::shared_ptr<Mesh> GameEntity::GetMesh()
+{
+	return std::shared_ptr<Mesh>();
+}
+
+std::shared_ptr<Transform> GameEntity::GetTransform()
+{
+	return std::shared_ptr<Transform>();
+}
+
+
+
--- GameEntity.h
+++ GameEntity.h
@@ -1,5 +1,19 @@
 #pragma once
+#include "Transform.h"
+#include "Mesh.h"
+#include <iostream>
+
 class GameEntity
 {
+public:
+	GameEntity(std::shared_ptr<Mesh> mesh_ptr);
+
+	// getters
+	std::shared_ptr<Mesh> GetMesh();
+	std::shared_ptr<Transform> GetTransform();
+
+private:
+	std::shared_ptr<Transform> myTransform;
+	std::shared_ptr<Mesh> myMesh;
 };
 
