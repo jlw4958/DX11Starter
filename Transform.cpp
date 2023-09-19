61628f1f22917e0de4372525fa79e17b15d2bc2c
Jabrecia Washington (RIT Student) <jlw4958@g.rit.edu>
2023-09-19 10:41:37 -0400

    small update

--- Transform.cpp
+++ Transform.cpp
@@ -1,18 +1,18 @@
 #include "Transform.h"
 #include <DirectXMath.h>
 
-Transform::Transform()
+Transform::Transform() : 
+	position(0, 0, 0),
+	pitchYawRoll(0, 0, 0),
+	scale(0, 0, 0)
 {
-	position(0, 0, 0);
-	pitchYawRoll(0, 0, 0);
-	scale(0, 0, 0);
 
 }
 
 DirectX::XMFLOAT4X4 Transform::GetWorldMatrix()
 {
 	DirectX::XMMATRIX t = DirectX::XMMatrixTranslation(position.x, position.y, position.z);
-	DirectX::XMMATRIX r = DirectX::XMMatrixRotationRollPitchYaw(XMLoadFloat3(&pitchYawRoll));
+	DirectX::XMMATRIX r = DirectX::XMMatrixRotationRollPitchYawFromVector(XMLoadFloat3(&pitchYawRoll));
 	DirectX::XMMATRIX s = DirectX::XMMatrixScalingFromVector(XMLoadFloat3(&scale));
 	
 }
