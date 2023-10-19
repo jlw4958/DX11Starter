c64ed71036749379b5d6dd21a31b904733ab6208
jlw4958 <jlw4958@g.rit.edu>
2023-10-19 16:43:15 -0400

    pixel shader WIP

--- CustomPixelShader.hlsl
+++ CustomPixelShader.hlsl
@@ -8,11 +8,25 @@ struct VertexToPixel
 
 	float4 screenPosition : SV_POSITION;
 	float2 uv : TEXTCOORD; 
+	float4 color : COLOR;        // RGBA color
 
 };
 
 float4 main(VertexToPixel input) : SV_TARGET
 {
-	return float4(1, 0, 1, 1);
+	VertexToPixel output;
+	
+	//float x = input.uv.x;
+	//float y = sin(x);
+	//output.uv = float2(x, y);
+	//if () {
+	//	output.color = float4(1, 0, 0, 1);
+	//}
+	//else {
+	//	output.color = float4(0, 0, 1, 1);
+	//}
 
+	//output.screenPosition = float4(sin(input.screenPosition.x), input.screenPosition.y, input.screenPosition.z, input.screenPosition.w);
+	//output.color = float4(1, 0, sqrt(sin(input.uv)));
+	return output.color;
 }
