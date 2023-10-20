31f7797944be7ee1d010779ae28997d71eb4c1fe
Jabrecia Washington <jlw4958@g.rit.edu>
2023-10-20 13:50:36 -0400

    figuring out how to affect UVs and color

--- CustomPixelShader.hlsl
+++ CustomPixelShader.hlsl
@@ -16,8 +16,9 @@ float4 main(VertexToPixel input) : SV_TARGET
 {
 	VertexToPixel output;
 	
-	//float x = input.uv.x;
-	//float y = sin(x);
+    //float x = input.uv.x;
+    //float y = sin(x) * 2;
+	
 	//output.uv = float2(x, y);
 	//if () {
 	//	output.color = float4(1, 0, 0, 1);
@@ -27,6 +28,18 @@ float4 main(VertexToPixel input) : SV_TARGET
 	//}
 
 	//output.screenPosition = float4(sin(input.screenPosition.x), input.screenPosition.y, input.screenPosition.z, input.screenPosition.w);
+	
+    //output.color = y * float4(1, 0, 0, 1);
+	
 	//output.color = float4(1, 0, sqrt(sin(input.uv)));
-	return output.color;
+	
+    output.uv.x = sin(input.uv.x);
+    output.uv.y = cos(input.uv.y);
+	
+    output.color = colorTint * output.uv.x * output.uv.y;
+
+    //output.color = float4(colorTint.x * sin(input.uv.x * 5), colorTint.y * cos(input.uv.y * 5), colorTint.z * tan(input.uv.x * 5), 1);
+
+	
+    return output.color;
 }
