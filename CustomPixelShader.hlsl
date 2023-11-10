6be30bade869d140668dc5311f9a28c9158b206b
Jabrecia Washington <jlw4958@g.rit.edu>
2023-11-10 18:46:46 -0500

    cleaned up + organized shaders

--- CustomPixelShader.hlsl
+++ CustomPixelShader.hlsl
@@ -2,7 +2,6 @@ cbuffer ExternalData : register(b0)
 {
     float4 colorTint;
     float totalTime;
-	//float greenValue;
 }
 
 struct VertexToPixel
@@ -15,40 +14,18 @@ struct VertexToPixel
 
 float4 main(VertexToPixel input) : SV_TARGET
 {
-	// UVs are usually between 0 and 1
-	// have colors change over time?
-	// how would we use this to draw a sine wave?
-	// how can we have the
-
     float4 color;
 
 	// get the number out of the 0 to 1 range and round down
     float solidNumU = trunc(input.uv.x * 10);
     float solidNumV = trunc(input.uv.y * 10);
+    
     if (solidNumV % 2)
     {
-
-		//if (greenValue < 1) {
-		//	greenValue* totalTime;
-		//}
-		//if (greenValue >= 1) {
-		//	greenValue / totalTime;
-		//}
-
-		//while (greenValue < 1.0f) {
-		//	greenValue *= totalTime;
-		//	color = float4(sin(input.uv.x), greenValue, .6f, 1.0f) * frac(sin(dot(input.uv, float2(12.9898, 78.233))) * 43758.5453123);
-		//}
-		//if (greenValue >= 1.0f) {
-		//	greenValue = 0.1f;
-		//}
-
         color = float4(sin(input.uv.x), input.uv.x * totalTime, .6f, 1.0f) * frac(sin(dot(input.uv, float2(12.9898, 78.233))) * 43758.5453123);
     }
     else
     {
-		// red
-		// add some noise!
         color = float4(cos(input.uv.y), 0.0f, .6f, 1.0f) * frac(cos(dot(input.uv, float2(12.9898, 78.233))) * 43758.5453123);
     }
 	
--- VertexShader.hlsl
+++ VertexShader.hlsl
@@ -26,7 +26,7 @@ VertexToPixel main(VertexShaderInput input)
 	output.worldPosition = mul(world, float4(input.localPosition, 1)).xyz; // local pos x world matrix (only first 3 components of the vector)
 	output.normal = mul((float3x3)worldInvTranspose, input.normal); // inverse transpose of world matrix cast to 3x3 x input normal
     output.uv = input.uv;
-    //output.tangent = input.tangent;
+    //output.color = float4(1, 1, 1, 1);
 
 	return output;
 }
